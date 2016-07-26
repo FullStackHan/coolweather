29623092172deeee6a5e315ac9b647f9128cc2f2
hanfanwu <2529386161@qq.com>
2016-07-26 16:56:40 +0800

    新增数据库帮助类，以及各种表对应的实体类。

---
+++ app/src/main/java/com/example/test/coolweather/model/Province.java
@@ -0,0 +1,37 @@
+package com.example.test.coolweather.model;
+
+/**
+ * Created by Administrator on 2016/7/26.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private  String provinceCode;
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
